20bc696398beb63902eb3717a4ef14beacc31df8
AmiteshSingh45 <amiteshsinghrajput00831@gmail.com>
2025-08-24 18:58:00 +0530

    added Ai-color-detection project

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../../public/images/logo.png"
+import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
