cf6fa9cf11ac85e71522bd50a1ea89f8f72cce54
AmiteshSingh45 <amiteshsinghrajput00831@gmail.com>
2026-05-25 14:05:57 +0530

    chore: update config files with comments and minor improvements

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+// Next.js configuration
+// Extend this object to add custom headers, redirects, rewrites, etc.
 const nextConfig = {}
 
 module.exports = nextConfig
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,6 @@
+// PostCSS configuration
+// Plugins: tailwindcss for utility classes, autoprefixer for cross-browser CSS support
+
 module.exports = {
   plugins: {
     tailwindcss: {},
